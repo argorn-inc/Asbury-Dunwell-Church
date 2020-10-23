31f8a1dfc3f25cd30c5d7daf61bf3d335fdfe9e0
samarithan <akotosel6@gmail.com>
2020-10-23 09:23:10 +0000

    automated 1

--- reserve/js/admin.js
+++ reserve/js/admin.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", () => {
   firebase
     .database()
-    .ref("ABCC/week7")
+    .ref("ABCC/currentWeek")
     .once("value", (snapshot) => {
       let array = [];
       snapshot.forEach((child) => {
@@ -33,7 +33,7 @@ function sortby(arg) {
   document.getElementById("tab").innerHTML = "";
   firebase
     .database()
-    .ref("ABCC/week7")
+    .ref("ABCC/currentWeek")
     .once("value", (snapshot) => {
       let array = [];
       snapshot.forEach((child) => {
@@ -44,18 +44,18 @@ function sortby(arg) {
       array.map((item, index) => {
         $("#tab").append(
           "<tr> <td style='text-align: center'>" +
-          item.seatNumber +
-          "</td><td style='text-align: center; text-transform: capitalize;'>" +
-          item.name +
-          "</td> <td style='text-align: center'>" +
-          item.service +
-          "</td> <td style='text-align: center'>" +
-          item.phone +
-          "</td>  <td style='text-align: center'>" +
-          item.city +
-          `</td> <td style="text-align: center">${new Date(
-            item.date
-          )}</td> </tr>`
+            item.seatNumber +
+            "</td><td style='text-align: center; text-transform: capitalize;'>" +
+            item.name +
+            "</td> <td style='text-align: center'>" +
+            item.service +
+            "</td> <td style='text-align: center'>" +
+            item.phone +
+            "</td>  <td style='text-align: center'>" +
+            item.city +
+            `</td> <td style="text-align: center">${new Date(
+              item.date
+            )}</td> </tr>`
         );
       });
     });
@@ -67,4 +67,118 @@ document.getElementById("print").addEventListener("click", () => {
     encodeURIComponent($("#tablewrap").html());
   location.href = url;
   return false;
+});
+
+document.getElementById("reset").addEventListener("click", () => {
+  console.log("reseting ...")
+  document.getElementById("reset").innerHTML = "Loading";
+  document.getElementById("reset").setAttribute("disabled", "true");
+  // alert("Database id reseting")
+  firebase
+    .database()
+    .ref("seatlayout")
+    .update({
+      seatlayout: JSON.stringify({
+        lay: [
+          1,
+          2,
+          3,
+          4,
+          5,
+          6,
+          7,
+          8,
+          9,
+          10,
+          11,
+          12,
+          13,
+          14,
+          15,
+          16,
+          17,
+          18,
+          19,
+          20,
+          21,
+          22,
+          23,
+          24,
+          25,
+          26,
+          27,
+          28,
+          29,
+          30,
+          31,
+          32,
+          33,
+          34,
+          35,
+          36,
+          37,
+          38,
+          39,
+          40,
+          41,
+          42,
+          43,
+          44,
+          45,
+          46,
+          47,
+          48,
+          49,
+          50,
+          51,
+          52,
+          53,
+          54,
+          55,
+          56,
+          57,
+          58,
+          59,
+          60,
+          61,
+          62,
+          63,
+          64,
+          65,
+          66,
+          67,
+          68,
+          69,
+          70,
+          71,
+          72,
+          73,
+          74,
+          75,
+          76,
+          77,
+          78,
+          79,
+          80,
+        ],
+      }),
+    });
+  firebase
+    .database()
+    .ref("ABCC/currentWeek")
+    .once("value", (snapshot) => {
+      firebase
+        .database()
+        .ref("ABCC/pastWeeks")
+        .push(snapshot.val())
+        .then(() => {
+          firebase.database().ref("ABCC/currentWeek").remove();
+        });
+    }).then(() => {
+      // alert("Database id reseting")
+      document.getElementById("reset").innerText = "Reset";
+      document.getElementById("reset").removeAttribute("disabled");
+    })
+
+ 
 });
--- reserve/js/index.js
+++ reserve/js/index.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", async () => {
   firebase
     .database()
-    .ref("ABCC/week7")
+    .ref("ABCC/currentWeek")
     .once("value", (snapshot) => {
       let arrayF = [];
       snapshot.forEach((child) => {
@@ -68,7 +68,7 @@ reg1.addEventListener("submit", (event) => {
 
   firebase
     .database()
-    .ref("ABCC/week7")
+    .ref("ABCC/currentWeek")
     .once("value", (snapshot) => {
       let array2 = [];
       snapshot.forEach((child) => {
@@ -78,7 +78,7 @@ reg1.addEventListener("submit", (event) => {
         ? run()
         : firebase
             .database()
-            .ref("ABCC/week7")
+            .ref("ABCC/currentWeek")
             .push()
             .set(
               {
@@ -137,94 +137,3 @@ reg1.addEventListener("submit", (event) => {
     });
 });
 
-let tesst = () => {
-  firebase
-    .database()
-    .ref("seatlayout")
-    .update({
-      seatlayout: JSON.stringify({
-        lay: [
-          1,
-          2,
-          3,
-          4,
-          5,
-          6,
-          7,
-          8,
-          9,
-          10,
-          11,
-          12,
-          13,
-          14,
-          15,
-          16,
-          17,
-          18,
-          19,
-          20,
-          21,
-          22,
-          23,
-          24,
-          25,
-          26,
-          27,
-          28,
-          29,
-          30,
-          31,
-          32,
-          33,
-          34,
-          35,
-          36,
-          37,
-          38,
-          39,
-          40,
-          41,
-          42,
-          43,
-          44,
-          45,
-          46,
-          47,
-          48,
-          49,
-          50,
-          51,
-          52,
-          53,
-          54,
-          55,
-          56,
-          57,
-          58,
-          59,
-          60,
-          61,
-          62,
-          63,
-          64,
-          65,
-          66,
-          67,
-          68,
-          69,
-          70,
-          71,
-          72,
-          73,
-          74,
-          75,
-          76,
-          77,
-          78,
-          79,
-          80,
-        ],
-      }),
-    });
-};
